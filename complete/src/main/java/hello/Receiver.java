6531e4252418ea8c63b6f7d13f54c562da91c7d6
Jon Brisbin <jon@jbrisbin.com>
2015-05-26 12:27:24 -0500

    Updated GSG to use latest Reactor 2.0

--- complete/src/main/java/hello/Receiver.java
+++ complete/src/main/java/hello/Receiver.java
@@ -1,26 +1,26 @@
 package hello;
 
-import java.util.concurrent.CountDownLatch;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import reactor.bus.Event;
+import reactor.fn.Consumer;
 
-import reactor.event.Event;
-import reactor.function.Consumer;
+import java.util.concurrent.CountDownLatch;
 
 @Service
 class Receiver implements Consumer<Event<Integer>> {
 
-    @Autowired
-    CountDownLatch latch;
-    
-    RestTemplate restTemplate = new RestTemplate();
+	@Autowired
+	CountDownLatch latch;
 
-    public void accept(Event<Integer> ev) {
-		QuoteResource quoteResource = restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/random", QuoteResource.class);
-        System.out.println("Quote " + ev.getData() + ": " + quoteResource.getValue().getQuote());
-        latch.countDown();
-    }
+	RestTemplate restTemplate = new RestTemplate();
+
+	public void accept(Event<Integer> ev) {
+		QuoteResource quoteResource =
+				restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/random", QuoteResource.class);
+		System.out.println("Quote " + ev.getData() + ": " + quoteResource.getValue().getQuote());
+		latch.countDown();
+	}
 
 }
